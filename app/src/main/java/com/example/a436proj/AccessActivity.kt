b6f4a8bc93de4479a3d14912a34d8adcda3245f7
MarcusWebDev <marcusbrookswebdev@gmail.com>
2022-10-26 18:37:57 -0400

    Added first screen and registration screen. Changed background color of Login screen and added some text at the bottom of Login screen.

---
+++ app/src/main/java/com/example/a436proj/AccessActivity.kt
@@ -0,0 +1,25 @@
+package com.example.a436proj
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.a436proj.databinding.ActivityAccessBinding
+import com.example.a436proj.databinding.ActivityLoginBinding
+
+class AccessActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAccessBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAccessBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSignIn.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+        binding.btnRegister.setOnClickListener {
+            startActivity(Intent(this, RegistrationActivity::class.java))
+        }
+    }
+}
--- app/src/main/java/com/example/a436proj/RegistrationActivity.kt
+++ app/src/main/java/com/example/a436proj/RegistrationActivity.kt
@@ -1,28 +1,29 @@
 package com.example.a436proj
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.content.Intent
 import com.example.a436proj.databinding.ActivityLoginBinding
-import com.example.a436proj.databinding.ActivityMainBinding
+import com.example.a436proj.databinding.ActivityRegistrationBinding
 
-class LoginActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityLoginBinding
+class RegistrationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegistrationBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnSignInSubmit.setOnClickListener {
+            val email = binding.emailText.text
             val userName = binding.usernameText.text
             val password = binding.passwordText.text
-            var authenticated = true
+            var validRegistration = true
 
-            //Begin code to handle sign in
+            //Begin code to handle registration. Things like checking that
+            //username is free and password is valid.
 
-            if(authenticated) {
+            if(validRegistration) {
                 startActivity(Intent(this, MainActivity::class.java))
                 finishAffinity()
             }
