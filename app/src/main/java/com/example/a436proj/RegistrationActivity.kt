4a41733eb8b548b2a6e880641fe65feea4724a97
Ahn <cahn11@montgomerycollege.edu>
2022-11-04 20:10:06 -0400

    google sign in

--- app/src/main/java/com/example/a436proj/RegistrationActivity.kt
+++ app/src/main/java/com/example/a436proj/RegistrationActivity.kt
@@ -3,30 +3,72 @@ package com.example.a436proj
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.a436proj.databinding.ActivityLoginBinding
+import android.view.View
+import android.widget.Toast
 import com.example.a436proj.databinding.ActivityRegistrationBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegistrationActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegistrationBinding
+    private var validator = Validators()
+    private lateinit var auth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegistrationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnSignInSubmit.setOnClickListener {
-            val email = binding.emailText.text
-            val userName = binding.usernameText.text
-            val password = binding.passwordText.text
-            var validRegistration = true
+        auth = requireNotNull(FirebaseAuth.getInstance())
 
-            //Begin code to handle registration. Things like checking that
-            //username is free and password is valid.
+        binding.btnSignInSubmit.setOnClickListener {registerNewUser()}
+    }
 
-            if(validRegistration) {
-                startActivity(Intent(this, MainActivity::class.java))
-                finishAffinity()
-            }
+    private fun registerNewUser(){
+        val email: String = binding.emailText.text.toString()
+        val password: String = binding.passwordText.text.toString()
+
+        if (!validator.validEmail(email)) {
+            Toast.makeText(
+                this,
+                getString(R.string.invalid_email),
+                Toast.LENGTH_LONG
+            ).show()
+
+            return
         }
+
+        if (!validator.validPassword(password)) {
+            Toast.makeText(
+                this,
+                getString(R.string.invalid_password),
+                Toast.LENGTH_LONG
+            ).show()
+
+            return
+        }
+        binding.progressBar.visibility = View.VISIBLE
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                binding.progressBar.visibility = View.GONE
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                        this,
+                        getString(R.string.register_success_string),
+                        Toast.LENGTH_LONG
+                    ).show()
+                    startActivity(Intent(this, LoginActivity::class.java))
+                    finishAffinity()
+                } else {
+                    Toast.makeText(
+                        this,
+                        getString(R.string.register_failed_string),
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+
+
     }
 }
