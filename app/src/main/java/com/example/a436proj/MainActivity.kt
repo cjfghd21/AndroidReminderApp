aa999eb28511e5b190dfea8ac838ad28a3d3e186
anthonyKim <anthony092777@gmail.com>
2022-11-06 21:08:24 -0500

    contact backend without permission

--- app/src/main/java/com/example/a436proj/MainActivity.kt
+++ app/src/main/java/com/example/a436proj/MainActivity.kt
@@ -1,23 +1,47 @@
 package com.example.a436proj
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.database.Cursor
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.a436proj.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    var cols = listOf<String>(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+        ContactsContract.CommonDataKinds.Phone.NUMBER,
+        ContactsContract.CommonDataKinds.Phone._ID,
+    ).toTypedArray()
+
+    @SuppressLint("Range")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(binding.root) //        setContentView(R.layout.activity_main)
+
+        var clr = findViewById<RecyclerView>(R.id.contact_list)
+        clr.layoutManager = LinearLayoutManager(this)
+
+
+
 
         val navView: BottomNavigationView = binding.navView
 
@@ -31,5 +55,56 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        // call read contacts
+        val btn_read_real = findViewById<Button>(R.id.btn_read_contact)
+        btn_read_real.setOnClickListener {
+            // verify permission
+            val contactList : MutableList<ContactDto> = ArrayList()
+            val contacts = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null)
+            if (contacts != null) {
+                while (contacts.moveToNext()) {
+                    val name = contacts.getString(contacts.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+                    val number = contacts.getString(contacts.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                    val obj = ContactDto()
+                    obj.name = name
+                    obj.number = number
+
+
+                    contactList.add(obj)
+                }
+                var clr = findViewById<RecyclerView>(R.id.contact_list)
+                clr.adapter = ContactAdapter(contactList, this)
+                contacts.close()
+            }
+        }
+    }
+
+
+    class ContactAdapter(items : List<ContactDto>, ctx: Context) : RecyclerView.Adapter<ContactAdapter.ViewHolder>(){
+
+        private var list = items
+        private var context = ctx
+
+        override fun getItemCount(): Int {
+            return list.size
+        }
+
+        override fun onBindViewHolder(holder: ContactAdapter.ViewHolder, position: Int) {
+            holder.name.text = list[position].name
+            holder.number.text = list[position].name
+
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapter.ViewHolder {
+            return ViewHolder(LayoutInflater.from(context).inflate(R.layout.contact_child,parent,false))
+        }
+
+
+        class ViewHolder(v: View) : RecyclerView.ViewHolder(v){
+            val name = this.itemView.findViewById<TextView>(R.id.tv_name)
+            val number = v.findViewById<TextView>(R.id.tv_number)
+
+        }
     }
 }
