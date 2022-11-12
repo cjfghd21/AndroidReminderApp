64446a240ccfa3a28714b8955e359d00ea0d2665
anthonyKim <anthony092777@gmail.com>
2022-11-11 23:32:18 -0500

    read contact is now fixed, should display name and number

--- app/src/main/java/com/example/a436proj/MainActivity.kt
+++ app/src/main/java/com/example/a436proj/MainActivity.kt
@@ -5,7 +5,6 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.database.Cursor
 import android.os.Bundle
 import android.provider.ContactsContract
 import android.view.LayoutInflater
@@ -18,7 +17,6 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.get
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -32,68 +30,12 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val PERMISSION = Manifest.permission.READ_CONTACTS
-        private const val CONTENT_ITEM_TYPE = ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_TYPE
+        const val CONTENT_ITEM_TYPE = ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_TYPE
     }
 
 
 
-    // registering permission launcher callback,
-    // asks grant permission and then call passed callback with grant status
-    // launched using ActivityResultLauncher.launch method
-    private val requestPermissionLauncher: ActivityResultLauncher<String> =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
-            isGranted ->
-                if (isGranted) {
-                    startContactsActivity()
-                } else {
-                    Toast.makeText(
-                        this,
-                        "cannot process without permission",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-        }
 
-    private val contactsLauncher: ActivityResultLauncher<Intent> =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            activityResult ->
-            startOurContactProcessing() ?:
-            Toast.makeText(this, "None selected", Toast.LENGTH_SHORT).show()
-        }
-
-    @SuppressLint("Range")
-    private fun startOurContactProcessing() {
-        val contactList : MutableList<ContactDto> = ArrayList()
-        val contacts = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null)
-        if (contacts != null) {
-            while (contacts.moveToNext()) {
-                val name = contacts.getString(contacts.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
-                val number = contacts.getString(contacts.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
-                val obj = ContactDto()
-                obj.name = name
-                obj.number = number
-
-
-                contactList.add(obj)
-            }
-            var clr = findViewById<RecyclerView>(R.id.contact_list)
-            clr.adapter = ContactAdapter(contactList, this)
-            contacts.close()
-        }
-    }
-
-    private fun startContactsActivity() {
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = CONTENT_ITEM_TYPE
-        intent.addFlags(Intent.FLAG_DEBUG_LOG_RESOLUTION)
-
-        // resolveActivity allow framework to determine
-        // app best suited to handle intent
-        // ask to choose btw candidate if needed
-        intent.resolveActivity(packageManager)?.let {
-            contactsLauncher.launch(intent)
-        }
-    }
 
     private lateinit var binding: ActivityMainBinding
 
@@ -102,16 +44,7 @@ class MainActivity : AppCompatActivity() {
         ContactsContract.CommonDataKinds.Phone._ID,
     ).toTypedArray()
 
-    private fun onReadButtonClick() {
-        when {
-            checkSelfPermission(PERMISSION) == PackageManager.PERMISSION_GRANTED -> {
-                startContactsActivity()
-            }
-            shouldShowRequestPermissionRationale(PERMISSION) -> {
-                requestPermissionLauncher.launch(PERMISSION)
-            }
-        }
-    }
+
 
 
     @SuppressLint("Range")
@@ -119,7 +52,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root) //        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
 
         var clr = findViewById<RecyclerView>(R.id.contact_list)
         clr.layoutManager = LinearLayoutManager(this)
@@ -140,58 +74,9 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        // call read contacts
-        val btn_read_real = findViewById<Button>(R.id.btn_read_contact)
-        btn_read_real.setOnClickListener {
-            //// new
-            onReadButtonClick()
-            //// below is old.. leave for temp
-            // verify permission
-//            val contactList : MutableList<ContactDto> = ArrayList()
-//            val contacts = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null)
-//            if (contacts != null) {
-//                while (contacts.moveToNext()) {
-//                    val name = contacts.getString(contacts.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
-//                    val number = contacts.getString(contacts.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
-//                    val obj = ContactDto()
-//                    obj.name = name
-//                    obj.number = number
-//
-//
-//                    contactList.add(obj)
-//                }
-//                var clr = findViewById<RecyclerView>(R.id.contact_list)
-//                clr.adapter = ContactAdapter(contactList, this)
-//                contacts.close()
-//            }
-        }
+
     }
 
 
-    class ContactAdapter(items : List<ContactDto>, ctx: Context) : RecyclerView.Adapter<ContactAdapter.ViewHolder>(){
 
-        private var list = items
-        private var context = ctx
-
-        override fun getItemCount(): Int {
-            return list.size
-        }
-
-        override fun onBindViewHolder(holder: ContactAdapter.ViewHolder, position: Int) {
-            holder.name.text = list[position].name
-            holder.number.text = list[position].name
-
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapter.ViewHolder {
-            return ViewHolder(LayoutInflater.from(context).inflate(R.layout.contact_child,parent,false))
-        }
-
-
-        class ViewHolder(v: View) : RecyclerView.ViewHolder(v){
-            val name = this.itemView.findViewById<TextView>(R.id.tv_name)
-            val number = v.findViewById<TextView>(R.id.tv_number)
-
-        }
-    }
 }
--- app/src/main/java/com/example/a436proj/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/a436proj/ui/home/HomeFragment.kt
@@ -1,26 +1,129 @@
 package com.example.a436proj.ui.home
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.a436proj.LoginActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.a436proj.ContactDto
+import com.example.a436proj.MainActivity
+import com.example.a436proj.R
 import com.example.a436proj.databinding.FragmentHomeBinding
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 class HomeFragment : Fragment() {
+
+    companion object {
+        private const val PERMISSION = Manifest.permission.READ_CONTACTS
+        private const val CONTENT_ITEM_TYPE = ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_TYPE
+    }
+
+
     private lateinit var oneTapClient: GoogleSignInClient
     private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private fun startContactsActivity() {
+        Log.d("CREATION", "startContactsActivity being called")
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = MainActivity.CONTENT_ITEM_TYPE
+        intent.addFlags(Intent.FLAG_DEBUG_LOG_RESOLUTION)
+
+        // resolveActivity allow framework to determine
+        // app best suited to handle intent
+        // ask to choose btw candidate if needed
+        activity?.packageManager?.let {
+            intent.resolveActivity(it)?.let {
+                contactsLauncher.launch(intent)
+            }
+        }
+        startOurContactProcessing() //////
+    }
+
+
+    private val contactsLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                activityResult ->
+            startOurContactProcessing() ?:
+            Toast.makeText(context, "None selected", Toast.LENGTH_SHORT).show()
+        }
+
+    @SuppressLint("Range")
+    private fun startOurContactProcessing() {
+        Log.d("CREATION", "startOutContactProcessing being called")
+        val contactList : MutableList<ContactDto> = ArrayList()
+        val contacts = activity?.contentResolver?.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null)
+        if (contacts != null) {
+            Log.d("CREATION", "Do we have non null contacts?")
+            while (contacts.moveToNext()) {
+                Log.d("CREATION", "How many contacts do we have")
+                val name = contacts.getString(contacts.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+                val number = contacts.getString(contacts.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                val obj = ContactDto()
+                obj.name = name
+                obj.number = number
+
+                Log.d("CREATION", obj.toString())
+                contactList.add(obj)
+            }
+            var clr = _binding?.contactList
+                //findViewById<RecyclerView>(R.id.contact_list)
+            Log.d("CREATION", contactList.toString())
+            clr?.adapter = context?.let { ContactAdapter(contactList, it) }
+            contacts.close()
+        }
+    }
+
+    private val requestPermissionLauncher: ActivityResultLauncher<String> =
+
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+                isGranted ->
+            if (isGranted) {
+                startContactsActivity()
+            } else {
+                Toast.makeText(
+                    context,
+                    "cannot process without permission",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+
+    private fun onReadButtonClick() {
+        Log.d("CREATION", "OnReadButtonClick enter")
+//        startContactsActivity()
+        requestPermissionLauncher.launch(PERMISSION)
+//        startContactsActivity()
+        when {
+            context?.let { checkSelfPermission(it, PERMISSION) } == PackageManager.PERMISSION_GRANTED -> {
+                Log.d("CREATION", "OnReadButtonClickFirst")
+                startContactsActivity()
+            }
+            shouldShowRequestPermissionRationale(PERMISSION) -> {
+                Log.d("CREATION", "OnReadButtonClickSecond")
+                requestPermissionLauncher.launch(PERMISSION)
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,9 +141,54 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+        val recyclerView: RecyclerView = binding.contactList
+
+        _binding!!.contactList.layoutManager = LinearLayoutManager(context)
+        _binding!!.btnReadContact.setOnClickListener{
+            onReadButtonClick()
+        }
+        Log.d("CREATION", "onCreateView is being called")
+
+
+
         return root
     }
 
+
+
+    ////////////////////////
+
+
+
+    class ContactAdapter(items : List<ContactDto>, ctx: Context) : RecyclerView.Adapter<ContactAdapter.ViewHolder>(){
+
+        private var list = items
+        private var context = ctx
+
+        override fun getItemCount(): Int {
+            return list.size
+        }
+
+        override fun onBindViewHolder(holder: ContactAdapter.ViewHolder, position: Int) {
+            holder.name.text = list[position].name
+            holder.number.text = list[position].number
+
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapter.ViewHolder {
+            return ViewHolder(LayoutInflater.from(context).inflate(R.layout.contact_child,parent,false))
+        }
+
+
+        class ViewHolder(v: View) : RecyclerView.ViewHolder(v){
+            val name = this.itemView.findViewById<TextView>(R.id.tv_name)
+            val number = v.findViewById<TextView>(R.id.tv_number)
+
+        }
+    }
+
+
+    ///////////
 
 
     override fun onDestroyView() {
