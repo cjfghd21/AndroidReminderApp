594f458b59c4ad54d7964199bc54285dd0199929
Ahn <cahn11@montgomerycollege.edu>
2022-11-21 01:29:45 -0500

    login persists

--- app/src/main/java/com/example/a436proj/AccessActivity.kt
+++ app/src/main/java/com/example/a436proj/AccessActivity.kt
@@ -1,17 +1,34 @@
 package com.example.a436proj
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.a436proj.databinding.ActivityAccessBinding
 import com.example.a436proj.databinding.ActivityLoginBinding
 
 class AccessActivity : AppCompatActivity() {
-
+    private lateinit var pref: SharedPreferences
     private lateinit var binding: ActivityAccessBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        pref = getSharedPreferences("Credentials", Context.MODE_PRIVATE)
+        if(pref.getString("email", "") != ""){
+
+            //testing purpose popup. can remove for final release.
+            Toast.makeText(
+                this,
+                "Login persisted.",
+                Toast.LENGTH_LONG
+            ).show()
+
+            startActivity(Intent(this, GroupActivity::class.java))
+        }
+
         binding = ActivityAccessBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
